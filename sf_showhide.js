f081e1f23ca115c3d68934e8fb035a25f2532296
Aurelien-UGent <101797641+Aurelien-UGent@users.noreply.github.com>
2022-03-17 10:15:29 +0100

    Update sf_showhide.js

--- sf_showhide.js
+++ sf_showhide.js
@@ -1,6 +1,6 @@
 // Sketchfab Viewer API: Start/Stop the viewer
 var version = "1.9.0";
-var uid = "try-d121c7acd55f4ba996a40dbb7b777cc5";
+var uid = "d121c7acd55f4ba996a40dbb7b777cc5";
 
 var urlParams = new URLSearchParams(window.location.search);
 var autoSpin = 0.0;
